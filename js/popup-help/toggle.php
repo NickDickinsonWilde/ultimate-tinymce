050867a6da516ccf489f2f06ad0a27bb168b0f43
josh401 <josh401@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-31 01:30:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ultimate-tinymce/tags/version 1.6.6@482628 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/popup-help/toggle.php
@@ -0,0 +1,44 @@
+<html><head>
+<title>Show/Hide Toggle Switch Box</title>
+<link rel=StyleSheet href="pop-upstyle.css" type="text/css" media="screen">
+</head>
+
+<BODY BGCOLOR="#F2F9FA" TEXT="#000000" LINK="#99CCFF" VLINK="#99CCFF" ALINK="#99CCFF" leftmargin=0 rightmargin=0 topmargin=0 bottommargin=0 marginheight=0 marginwidth=0>
+
+<!-- OUTER TABLE-->
+<TABLE cellpadding=0 cellspacing=0 border="0" bordercolor="666666" width="100%" height="100%"><tr><td ALIGN="CENTER" VALIGN="TOP">
+
+<!-- CONTENT TABLE -->
+<TABLE cellpadding=0 cellspacing=0 border="0" bordercolor="666666" width="80%"><tr><td ALIGN="left" VALIGN="TOP">
+<br>
+
+<span class="title" style="padding:0px 5px 0px 5px;background-color:#CCFCD9;border:1px solid #339966;"><strong>Show/Hide Toggle Switch Box:</strong></span>
+<br />
+This option will enable the Show/Hide Toggle Switch Box.  This button will be added to row four of the visual editor.
+
+<br /><br />
+
+<span class="title" style="padding:0px 5px 0px 5px;background-color:#CCFCD9;border:1px solid #339966;"><strong>Usage:</strong></span>
+<br />
+Placing a check in the box will enable toggle switch.  Unchecking the box removes toggle switch.<br /><br />
+This is a nifty little javascript which will add a toggle switch just above the visual editor.  It will "quickly" show or hide the editor buttons when clicked.  I decided this is most often used in brief instances, so I decided to make it only during a page "stay".  Once you refresh the page, or leave and come back, all the visual editor button rows will again be displayed by default.
+
+<br /><br />
+<img src="images/toggle.png" border="1px solid #000000" /><br />
+<em>Shows the toggle switch to show/hide the editor buttons.</em><br /><br />
+
+</td></tr></table>
+<!-- CONTENT TABLE -->
+
+</td></tr><tr><td ALIGN="center" VALIGN="bottom"><!-- OUTER TABLE -->
+
+<!-- CLOSE BUTTON -->
+<form>
+<input type=button value="Close Window" onClick='self.close()'>
+</form>
+
+</td></tr></table>
+<!-- OUTER TABLE-->
+
+</BODY>
+</HTML>
