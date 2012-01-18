2da67c8de4364498cc1dd968365bb8cd5714255c
josh401 <josh401@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-18 09:11:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ultimate-tinymce/tags/version 1.6.8@491593 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/popup-help/advlink.php
@@ -0,0 +1,47 @@
+<html><head>
+<title>Advanced Insert/Edit Link</title>
+<link rel=StyleSheet href="pop-upstyle.css" type="text/css" media="screen">
+</head>
+
+<BODY BGCOLOR="#F2F9FA" TEXT="#000000" LINK="#99CCFF" VLINK="#99CCFF" ALINK="#99CCFF" leftmargin=0 rightmargin=0 topmargin=0 bottommargin=0 marginheight=0 marginwidth=0>
+
+<!-- OUTER TABLE-->
+<TABLE cellpadding=0 cellspacing=0 border="0" bordercolor="666666" width="100%" height="100%"><tr><td ALIGN="CENTER" VALIGN="TOP">
+
+<!-- CONTENT TABLE -->
+<TABLE cellpadding=0 cellspacing=0 border="0" bordercolor="666666" width="80%"><tr><td ALIGN="left" VALIGN="TOP">
+<br>
+
+<span class="title" style="padding:0px 5px 0px 5px;background-color:#CCFCD9;border:1px solid #339966;"><strong>Advanced Insert/Edit Link:</strong></span>
+<br />
+This option will enable the Advanced Insert/Edit Link.
+
+<br /><br />
+
+<span class="title" style="padding:0px 5px 0px 5px;background-color:#CCFCD9;border:1px solid #339966;"><strong>Usage:</strong></span>
+<br />
+Checking this option will replace the original default wordpress functionality of the insert/edit link box.  Unchecking this option will restore the default wordpress functionality.<br /><br />
+The advanced version gives you many more options for your links.  You can create javascripts, mouseovers and mouseouts, and plenty of other advanced options.  This is all done through a nifty GUI experience.
+
+<br /><br />
+
+<img src="images/advanced-link-enabled.png" border="1px solid #000000" /><br />
+<em>View of advanced insert/edit link box.</em><br /><br />
+<img src="images/advanced-link-disabled.png" border="1px solid #000000" /><br />
+<em>View of the WordPress default insert/edit link box.</em><br /><br />
+
+</td></tr></table>
+<!-- CONTENT TABLE -->
+
+</td></tr><tr><td ALIGN="center" VALIGN="bottom"><!-- OUTER TABLE -->
+
+<!-- CLOSE BUTTON -->
+<form>
+<input type=button value="Close Window" onClick='self.close()'>
+</form>
+
+</td></tr></table>
+<!-- OUTER TABLE-->
+
+</BODY>
+</HTML>
